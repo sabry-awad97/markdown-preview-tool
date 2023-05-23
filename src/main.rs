dfc9f0e8b0ac0d24f084c1d7da51ec1470e2141f
Sabry Awad <dr.sabry1997@gmail.com>
2023-05-23 13:40:19 +0300

    🎉 feat(main.rs): Add command-line argument parsing using StructOpt
    The StructOpt crate is introduced to handle command-line argument parsing for the Markdown preview tool. This allows users to specify a Markdown file to preview.
    
    Changes made:
    - Added the `structopt` crate as a dependency.
    - Created a new struct `Opt` using the `derive` attribute `StructOpt` to define the command-line options and provide additional information.
    - Added a `file` field of type `String` to the `Opt` struct, representing the Markdown file to preview.
    - Updated the `main` function to create an instance of `Opt` using `Opt::from_args()`.
    
    Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Markdown Preview Tool",
+    about = "Converts Markdown to HTML and opens it in a web browser for preview."
+)]
+struct Opt {
+    #[structopt(short, long, help = "Markdown file to preview")]
+    file: String,
+}
+
 fn main() {
-    println!("Welcome to the markdown preview tool!");
+    let opt = Opt::from_args();
+    
 }
